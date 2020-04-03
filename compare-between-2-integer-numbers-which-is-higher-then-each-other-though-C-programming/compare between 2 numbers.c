cf70fff6222f7d616296d0d321ee6729ea8685a4
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-04-04 05:29:15 +0600

    Create compare between 2 numbers.c

---
+++ compare-between-2-integer-numbers-which-is-higher-then-each-other-though-C-programming/compare between 2 numbers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int
+main ()
+{
+  int a, b, sum;
+
+
+    printf ("Enter the 1st number\n");
+
+
+scanf ("%d", &a);
+
+printf ("Enter the 2nd number\n");
+
+
+scanf ("%d", &b);
+
+ if(a<b)
+{
+    printf("B is hiigher then A\n",b);
+
+}
+else
+printf ("A is higher number then B\n", a);
+
+
+return 0;
+
+}
