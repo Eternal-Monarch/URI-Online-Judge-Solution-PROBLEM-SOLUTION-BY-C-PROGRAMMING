e9cf46a582d7afe07bba7666f5f29e784baf83ac
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-04-04 05:26:50 +0600

    Create Distance Between Two Points.c

---
+++ Distance Between Two Points.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{ 
+double  a,b,c,d,x,y,r,w;
+
+
+
+
+
+scanf("%lf",& a);
+scanf("%lf",& b);
+scanf("%lf",& c);
+scanf("%lf",& d);
+ 
+x=(a-c);
+y=(b-d);
+
+r=(x*x)+(y*y);
+
+w=sqrt(r);
+
+
+
+
+
+
+
+
+ {
+
+printf("%.4lf",w);
+}
+return 0;
+
+}
