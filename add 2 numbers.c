0a63d27c4cc92bb53ba6672a29716799745fa5c9
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-04-04 05:31:52 +0600

    Create add 2 numbers.c

---
+++ add 2 numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int
+main ()
+{
+  int a, b, sum;
+
+    //printf("Enter the 1st number\n");
+    printf ("Enter the 1st number\n");
+
+
+scanf ("%d", &a);
+
+printf ("Enter the 2nd number\n");
+
+
+scanf ("%d", &b);
+
+sum = a + b;
+
+printf ("The sum is %d\n", sum);
+
+
+return 0;
+
+}
