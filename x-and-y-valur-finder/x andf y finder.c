3b94c15e6527f8ceddd9788d0d1250bdef562bd3
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-04-04 05:25:30 +0600

    Create x andf y finder.c

---
+++ x-and-y-valur-finder/x andf y finder.c
@@ -0,0 +1,16 @@
+  
+#include <stdio.h>
+
+int
+main ()
+{int a,b,sum,mul;
+ printf("enter x+y\n");
+ printf("enter x-y\n");
+ scanf("%d%d",&a,&b);
+sum=(a+b)/2;
+mul=(a-b)/2;
+ printf("x is %d\n",sum);
+
+printf("y is %d\n",mul);
+return 0;
+}
