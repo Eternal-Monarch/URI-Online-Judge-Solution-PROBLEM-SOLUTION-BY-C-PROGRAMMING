b4fa2d95b623e8ae349897beb4a2dcf701cfca70
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-04-04 09:18:09 +0600

    Create URI PROBLEM SOLUTION IN C OF 1008.c

---
+++ URI PROBLEM SOLUTION IN C OF 1008.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{   
+int a,b;
+float MEDIA;
+float x;
+//printf("enter the employes number:\n");
+//printf("His/Her worked hours number in a month :\n");
+   //double A,B,c,d,MEDIA;
+   scanf("%d", & a);
+   scanf("%d",& b);
+   scanf("%f",& x);
+   //scanf("%lf",&d);
+   MEDIA = (b*x);
+   //x=MEDIA/3;
+   //y=(A*2+B*3+c*5)/(10);
+  // printf("DIFERENCA = %.0lf\n",MEDIA);
+
+
+
+printf("NUMBER = %d\n",a);
+printf("SALARY = U$ %.2lf\n",MEDIA);
+
+return 0;
+}
+
