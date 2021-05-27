924c6353cfe78e228433521fe102432f72e7d3f3
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-05-27 13:43:37 +0600

    Create URI Online Judge | 1114 Fixed Password.c

---
+++ URI Online Judge | 1114 Fixed Password.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int
+main ()
+{
+    int a,b;
+   while(1)
+   {
+       scanf("%d",&a);
+       if(a==2002)
+       {
+
+           printf("Acesso Permitido\n");
+       }
+       else{
+        printf("Senha Invalida\n");
+       }
+   }
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
