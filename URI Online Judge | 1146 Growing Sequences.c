d20a27a876285867e62808e14a6f9a112fdf756a
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-06-04 02:23:26 +0600

    Create URI Online Judge | 1146 Growing Sequences.c

---
+++ URI Online Judge | 1146 Growing Sequences.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    int x=1;
+    int i,j;
+    int line;
+while(1)
+{
+
+   scanf("%d",&a);
+     if(a>0)
+    {
+        for(i=1;i<=a;i++)
+
+        printf("%d ",i);
+        printf("\n");
+
+     }
+     else if(a==0)
+{
+
+
+    break;
+}
+
+    }
+return 0;
+
+}
