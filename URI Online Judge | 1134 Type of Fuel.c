4d6615850de432f3b6ef26e04874fb220611edd0
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-06-03 08:03:00 +0600

    Create URI Online Judge | 1134 Type of Fuel.c

---
+++ URI Online Judge | 1134 Type of Fuel.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int i;int n;
+int a=0,g=0,d=0;
+printf("MUITO OBRIGADO\n");
+
+while(1)
+{{
+    scanf("%d",&n);
+
+    {
+        if(n==1)
+       {
+
+       a++;
+       }
+    else if(n==2)
+       {
+
+        g++;
+       }
+    else if(n==3)
+       {
+d++;
+       }
+
+}
+}
+if(n==4)
+{
+    break;
+}}
+printf("Alcool: %d\n",a);
+printf("Gasolina: %d\n",g);
+printf("Diesel: %d\n",d);
+
+
+
+return 0;
+}
