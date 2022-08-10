2be62df2f7d5573e969611056e2ee6d10371c02f
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-08-10 15:41:53 +0530

    Create URI Online Judge | 1164 Perfect Number.c

---
+++ URI Online Judge | 1164 Perfect Number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a;
+int b;
+int i;
+scanf("%d",&a);//user input
+for(i=0;i<a;i++)//loop
+{
+scanf("%d",&b);
+{//user given input number
+if(b%2==0)
+{printf("%d eh perfeito\n",b);
+};
+ if(b%2!=0)
+
+{
+printf("%d nao eh perfeito\n",b);
+}
+}
+}
+return 0;
+}
