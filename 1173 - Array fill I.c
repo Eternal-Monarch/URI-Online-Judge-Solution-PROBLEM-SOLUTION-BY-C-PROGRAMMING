7609e90b75840bc268f71e318073f3b446fdc537
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-10-08 02:09:20 +0530

    Create  1173 - Array fill I.c

---
+++ 1173 - Array fill I.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    int number,i,x,N[10];
+
+    scanf("%d",&number);
+
+    for(i = 0; i < 10; i++)
+    {
+
+        printf("N[%d] = %d\n",i,number);
+number=number*2;
+
+
+    }
+
+    return 0;
+
+}
