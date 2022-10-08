a13a9bc6fa02b3129e23ab1978863d1a5e7d2427
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2022-10-08 14:20:20 +0530

    Create  1175 - Array change I.c

---
+++ 1175 - Array change I.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int number,i,j;
+int array[20];
+for(i=19;i>=0;i--)
+{
+ scanf("%d",&array[i]);
+}
+for(i=0;i<20;i++)
+
+   {
+       printf("N[%d] = %d\n",i,array[i]);
+   }
+    //array
+
+
+
+return 0;
+}
